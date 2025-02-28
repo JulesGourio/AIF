1928eea449596c5fde04b613df5b633009246126
JulesGourio <jules.gourio@etu.inp-n7.fr>
2025-02-28 12:39:27 +0100

    push git

--- developpement/MNIST/mnist_webapp.py
+++ developpement/MNIST/mnist_webapp.py
@@ -4,13 +4,21 @@ import requests
 import io
 
 
+import io
+import requests
+from PIL import Image
+import numpy as np
+
 def recognize_digit(image):
-    # Convert to PIL Image necessary if using the API method
+    image = image['composite']
     image = Image.fromarray(image.astype('uint8'))
-    img_binary = ...
-    ...
-    ...
-    return ....
+    image = image.resize((28, 28))
+    image = image.convert('L')
+    images_binary = io.BytesIO()
+    image.save(images_binary, format='PNG')
+    reponse = requests.post('http://127.0.0.1:5000/predict', data=images_binary.getvalue())
+    return reponse.json()['prediction']
+
 
 if __name__=='__main__':
 
